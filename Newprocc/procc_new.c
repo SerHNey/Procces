260c236dee26957449e43c9458afa51c46613076
SerHNey <naumov.sergey2003@gmail.com>
2022-11-27 21:19:55 +0300

    Добавьте файлы проекта.

---
+++ Newprocc/procc_new.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+#include <math.h>
+DWORD countreadbytes = 100;
+DWORD readingbytes = 100;
+int main(int argc, char* argv[]) {
+	char* stroka = argv[0];
+
+	
+	HANDLE file = CreateFile(L"result.txt", GENERIC_WRITE, NULL, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	int i = 0;
+	while (stroka[i]) {
+		i++;
+		if (stroka[i] > 96 || stroka[i] < -1) {
+			sprintf(stroka, "Ошибка данных2");
+			readingbytes = strlen(stroka);
+			WriteFile(file, stroka, readingbytes, &readingbytes, NULL);
+			return 1;
+		}
+	}
+	char* number;
+	number = strtok(stroka, " ");
+	double numbers[3];
+	numbers[0] = atof(number);
+	i = 1;
+	while (number != NULL)
+	{
+		number = strtok(NULL, " ");
+		if (number != NULL) {
+			if (number[0] != '\r') {
+				numbers[i] = atof(number);
+				i++;
+			}
+		}
+		if (i > 3)
+			break;
+	}
+	double a = -3; double b = -14; double c = 5;
+	double diskrimenant = sqrt(pow(b, 2) - 4 * a * c);
+	if (diskrimenant < 0) {
+		sprintf(stroka, "Дискриминант меньше 0\n Корней нет");
+		readingbytes = strlen(stroka);
+		WriteFile(file, stroka, readingbytes, &readingbytes, NULL);
+	}
+	if (diskrimenant == 0) {
+		c = (-b) / (2 * a);
+		sprintf(stroka, "Дискрименант равен = 0\n x1 = %f\n x2 = %f", c, c);
+		readingbytes = strlen(stroka);
+		WriteFile(file, stroka, readingbytes, &readingbytes, NULL);
+	}
+	if (diskrimenant > 0) {
+		c = (-b + diskrimenant) / (2 * a);
+		a = (-b - diskrimenant) / (2 * a);
+		sprintf(stroka, "Корень дискрименанта равен = %f\n x1 = %f\n x2 = %f", diskrimenant, c, a);
+		readingbytes = strlen(stroka);
+		WriteFile(file, stroka, readingbytes, &readingbytes, NULL);
+	}
+	CloseHandle(file);
+	return 1;
+}
---
+++ Procces/Procc_.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+int countsymbls(char* namefile);
+
+
+int main() {
+	system("chcp 1251>nul");
+	STARTUPINFO si = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	int size = countsymbls("data.txt");
+	HANDLE file = CreateFile(L"data.txt", GENERIC_READ, NULL, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file != INVALID_HANDLE_VALUE) {
+		DWORD countreadbytes = 100;
+		DWORD readingbytes = 100;
+		char* stroka = calloc(size, sizeof(char));
+		if (!(ReadFile(file, stroka, size, &readingbytes, NULL))) {
+			file = CreateFile(L"result.txt", GENERIC_READ, NULL, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			sprintf(stroka, "Ошибка");
+			WriteFile(file, stroka, strlen(stroka), &readingbytes, NULL);
+			return 1;
+		}
+		CloseHandle(file);
+		int procc = CreateProcessA("..\\x64\\Debug\\Newprocc.exe",stroka, NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi);
+	
+		WaitForSingleObject(pi.hProcess, INFINITE);
+		DWORD dwCode;
+		GetExitCodeProcess(pi.hProcess, &dwCode);
+
+		if (dwCode!= 1) {
+			size = countsymbls("result.txt");
+			stroka = calloc(countsymbls("result.txt"), sizeof(char));
+
+			HANDLE filee = CreateFile(L"result.txt", GENERIC_READ, NULL, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			ReadFile(filee, stroka, size, &readingbytes, NULL);
+			printf("%s", stroka);
+		}
+	}
+}
+
+int countsymbls(char* namefile) {
+	FILE* pFile = fopen(namefile, "r");
+	int size = 0;
+	if (!pFile) {
+		perror("Error opening file");
+		return 0;
+	}
+	else
+	{
+		fseek(pFile, 0, SEEK_END);
+		size = ftell(pFile);
+		fclose(pFile);
+		return size;
+	}
+}
